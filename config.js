69e81ba12886316ee4cd83e042c9266a320c5164
Alex Bowen <alexbowen@Alexs-iMac.local>
2018-06-03 00:10:52 -0700

    removed gitignore

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'Ce561GJjzbffe0eGZBjiFFqpY',
+  consumer_secret:      'ssh0TnH6ctX8J4ODA9Uzo8s1Fy3Z8TszlqAffphXFavKtnciIF',
+  access_token:         '1003159974128652288-e4eVp2Gih2BGcKJ2W6g3Vw9wbP0d91',
+  access_token_secret:  'dlJITKKRAPcuuKsOYAjj6kub7csfwpGSVNfg8yr9CdK4i',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
